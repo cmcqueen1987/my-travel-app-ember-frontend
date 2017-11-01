e9a20abf13e91b5c8e269d0b65e2dab0e0535ea6
cmcqueen1987 <charlie.mcqueen87@gmail.com>
2017-11-01 15:09:17 -0400

    Ember Template
    made file changes based on directions. Commit before deployment.

--- config/environment.js
+++ config/environment.js
@@ -2,11 +2,12 @@
 
 module.exports = function (environment) {
   'use strict';
-  const ENV = {
-    modulePrefix: 'ga-wdi-boston.ember-auth',
+  var ENV = {
+    modulePrefix: 'my-travel-app',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
+    apiHost: 'http://localhost:3000/',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -49,7 +50,9 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
+    ENV.rootURL = '/my-travel-app-ember-frontend';
     ENV.locationType = 'hash';
+    ENV.apiHost = 'https://my-travel-app-capstone.herokuapp.com/';
   }
 
   return ENV;
