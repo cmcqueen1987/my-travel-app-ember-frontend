c699267044f4dce0a79875e5f1af622fa1a52112
cmcqueen1987 <charlie.mcqueen87@gmail.com>
2017-11-03 12:07:15 -0400

    Created country.js route
    Injected flash message service through ember library
    defined create country action
    created function to save new country

--- app/routes/country.js
+++ app/routes/country.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model (params) {
+    return this.get('store').findRecord('country', params.country_id);
+  },
+  actions: {
+
+  },
 });
