e48d9d69188507be770b19230ffe043cd97defcb
cmcqueen1987 <charlie.mcqueen87@gmail.com>
2017-11-03 14:47:36 -0400

    created action for toggling country status on click

--- app/components/country-list.js
+++ app/components/country-list.js
@@ -4,6 +4,13 @@ export default Ember.Component.extend({
   classNames: ['country'],
   classNameBindings: ['countryDetailHidden'],
   countryDetailHidden: false,
-
+  actions: {
+  toggleCountryDetail () {
+        return this.toggleProperty('countryDetailHidden');
+      },
+      toggleCountryDone (country) {
+        return this.sendAction('toggleCountryDone', country);
+      }
+      },
 
 });
