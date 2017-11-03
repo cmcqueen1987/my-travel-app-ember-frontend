7d6c92b45fc40167746a389ac941993851c3c614
cmcqueen1987 <charlie.mcqueen87@gmail.com>
2017-11-03 15:05:00 -0400

    toggle detail done in country-list/card.js

--- app/components/country-list/card.js
+++ app/components/country-list/card.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'li',
+classNameBindings: ['countryCompleted'],
+countryCompleted: Ember.computed.alias('country.done'),
+actions: {
+  toggleDone () {
+    return this.sendAction('toggleCountryDone', this.get('country')); //send up to shopping list component
+  },
   actions: {
     deleteCountry () {
     return this.sendAction('deleteCountry', this.get('country'));
